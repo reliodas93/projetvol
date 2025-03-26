1167eb623bbedf4b4204a8cf0c93d87eb844a933
Reyan BABOU <BABOU_Re@lprs.local>
2025-03-26 11:43:20 +0100

    [fix] inscription et connexion fonctionelle

--- src/traitement/trait_connexion.php
+++ src/traitement/trait_connexion.php
@@ -9,29 +9,34 @@ if (!isset($_POST["email"]) || !isset($_POST["mot_de_passe"]) || empty($_POST["e
     echo "C'est pas bien, vous avez laissé une case vide";
     header("Location: ../../vue/ConnexionEM.php");
     exit();
-}
-
-$email = $_POST["email"];
-$motDePasse = $_POST["mot_de_passe"];
-
-$utilisateurRepo = new UtilisateursRepository();
-$utilisateur = $utilisateurRepo->connexionUtilisateurs($email, $motDePasse);
-
-if ($utilisateur) {
-    session_start();
-
-    $_SESSION['id_utilisateur'] = $utilisateur->getIdUtilisateur();
-    $_SESSION['nom'] = $utilisateur->getNom();
-    $_SESSION['prenom'] = $utilisateur->getPrenom();
-    $_SESSION['email'] = $utilisateur->getemail();
-    $_SESSION['date_de_naissance'] = $utilisateur->getDateDeNaissance();
-    $_SESSION['ville_de_naissance'] = $utilisateur->getVilleDeNaissance();
-
-    header("Location: ../../vue/AccueilEM.php");
-    exit();
 } else {
-    session_start();
-    session_destroy();
-    header("Location: ../../vue/ConnexionEM.php");
-    exit();
+    $utilisateur = new Utilisateurs([
+        'email' => $_POST["email"],
+        'motDePasse' => $_POST["mot_de_passe"]
+    ]);
+
+    $email = $_POST["email"];
+    $motDePasse = $_POST["mot_de_passe"];
+
+    $utilisateursRepository = new UtilisateursRepository();
+    $resultat = $utilisateursRepository->connexionUtilisateurs($utilisateur);
+
+    if ($utilisateur) {
+        session_start();
+
+        $_SESSION['id_utilisateur'] = $utilisateur->getIdUtilisateur();
+        $_SESSION['nom'] = $utilisateur->getNom();
+        $_SESSION['prenom'] = $utilisateur->getPrenom();
+        $_SESSION['email'] = $utilisateur->getemail();
+        $_SESSION['date_de_naissance'] = $utilisateur->getDateDeNaissance();
+        $_SESSION['ville_de_naissance'] = $utilisateur->getVilleDeNaissance();
+
+        header("Location: ../../vue/AccueilEM.php");
+        exit();
+    } else {
+        session_start();
+        session_destroy();
+        header("Location: ../../vue/ConnexionEM.php");
+        exit();
+    }
 }
